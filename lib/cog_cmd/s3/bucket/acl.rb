68e45246b6ef3e64609e76ceb713f5f759bbd0e5
vanstee <patrick@vanstee.me>
2016-10-26 09:57:42 -0400

    Add descriptions and fix options

--- lib/cog_cmd/s3/bucket/acl.rb
+++ lib/cog_cmd/s3/bucket/acl.rb
@@ -5,21 +5,21 @@ module CogCmd::S3::Bucket
   class Acl < S3::Command
     def run_command
       require_valid_region!
-      require_name!
+      require_bucket!
 
-      acls = client.list_bucket_acls(name)
+      acls = client.list_bucket_acls(bucket)
 
       response.template = 'bucket_acl'
       response.content = acls.map(&:to_h)
     end
 
-    def require_name!
-      unless name
-        raise(Cog::Error, 'Name not provided. Provide a name as the first argument.')
+    def require_bucket!
+      unless bucket
+        raise(Cog::Error, 'Bucket not provided. Provide a bucket as the first argument.')
       end
     end
 
-    def name
+    def bucket
       request.args.first
     end
   end
